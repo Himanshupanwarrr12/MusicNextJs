62678b6df2b444e7f3cafa98711e956c27c4e76c
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-21 20:09:30 +0530

    added  div outside navbar and gave style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar/>
+        <div>
+          <NavBar className="relative w-full flex items-center justify-center" />
+        </div>
+
         {children}
       </body>
     </html>
