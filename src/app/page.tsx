e4f697b405772e048250d6574060597c72ad19ce
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-29 02:01:46 +0530

    feat : HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import HeroSection from "@/components/ui/HeroSection";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home(){
 return(
   <>
   <h1 className="text-4xl text-center" >Himanshu</h1>
-  {/* <HeroSection  /> */}
+  <HeroSection  />
   </>
 )
 }
