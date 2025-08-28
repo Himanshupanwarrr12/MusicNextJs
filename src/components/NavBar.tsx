472eeb522f778bf3009f8048a37a42b9b57613b6
Himanshu Panwar <hpanwar875@gmail.com>
2025-08-29 02:02:41 +0530

    feat : Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import React, { act, useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import React, {  useState } from "react";
+import { HoveredLink, Menu, MenuItem} from "./ui/navbar-menu";
 import { cn } from "../lib/utils/util";
 import Link from "next/link";
-import Image from "next/image";
 
 function NavBar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
 
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl border mx-auto z-50",)}
+      className={cn("fixed top-10 inset-x-0 max-w-2xl  mx-auto z-50",)}
     > 
       <Menu setActive={setActive}   >
         <Link href={"/"}>
           <MenuItem setActive={setActive} active={active} item="Home">
-            This is home
+            
           </MenuItem>
         </Link>
         <MenuItem setActive={setActive} active={active} item="Our Courses">
